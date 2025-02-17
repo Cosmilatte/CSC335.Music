9ab159456eefba74b892dec76fc957f3851e91bd
Cosmilatte <166539222+Cosmilatte@users.noreply.github.com>
2025-02-17 13:25:22 -0700

    Add Artist.java Class
    Artist will store Albums which will store Songs.

---
+++ Artist.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.ArrayList;
+
+// Artist.java
+// Created 2 - 17 - 2025
+// Authors: Lilian and Lucian
+// Purpose: 
+
+public class Artist
+{
+	// INSTANCE VARIABLES
+	ArrayList<Album> albums;
+	String name;
+	
+	// CONSTRUCTOR
+	public Artist(String name)
+	{
+		this.albums = new ArrayList<Album>();
+		this.name = name;
+	}
+	
+	
+	// GETTERS+SETTERS
+	
+	
+	// MISC. METHODS
+	public void addAlbum(Album a)
+	{
+		if ((a.getArtist()).compareTo(name) != 0)
+		{
+			System.err.println("ERROR: Album's artist does not align with artist");
+			System.err.println("Album: " + a.getArtist() + " VS: " + name);
+		}
+		else
+		{
+			albums.add(a);
+		}
+	}
+}
