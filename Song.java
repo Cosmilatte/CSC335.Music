031e28d5f8f402b0da570c453b5f1e9a31ba7c05
Cosmilatte <166539222+Cosmilatte@users.noreply.github.com>
2025-02-17 13:23:35 -0700

    Update Song.java
    This is mostly complete minus any methods necessary for the required utilities.

--- Song.java
+++ Song.java
@@ -8,10 +8,52 @@ package model;
 public class Song
 {
 	// INSTANCE VARIABLES
+	private String title;
+	private String artist;
+	private int rating;
+	
 	
 	// CONSTRUCTOR
+	public Song(String title, String artist)
+	{
+		this.title = title;
+		this.artist = artist;
+		
+		this.rating = 0;
+	}
+	
 	
 	// GETTERS+SETTERS
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	
+	public String getArtist()
+	{
+		return artist;
+	}
+	
+	
+	public int getRating()
+	{
+		return rating;
+	}
+	
+	
+	public void setRating(int r)
+	{
+		if ((r > 5) || (r < 1))
+		{
+			System.err.println("ERROR: Rating number must be betweem 1 - 5, was " + r);
+		}
+		else
+		{
+			this.rating = r;
+		}
+	}
+	
 	
 	// MISC. METHODS
 }
