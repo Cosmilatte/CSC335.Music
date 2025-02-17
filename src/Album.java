bdaa15d5cd6248a79ee54776f01eb42f7f26f902
lix2_an_wei <931115lilian@gmail.com>
2025-02-16 17:02:52 -0700

    Create Album.java
    Testing to link github repository to eclipse

---
+++ src/Album.java
@@ -0,0 +1,17 @@
+package model;
+
+// Album.java
+// Created 2 - 15 - 2025
+// Authors: Lilian and Lucian
+// Purpose: 
+
+public class Album
+{
+	// INSTANCE VARIABLES
+	
+	// CONSTRUCTOR
+	
+	// GETTERS+SETTERS
+	
+	// MISC. METHODS
+}
