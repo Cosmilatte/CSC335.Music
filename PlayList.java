5c189f065f2772c03f4ea205361fd3e0d294eded
lix2_an_wei <931115lilian@gmail.com>
2025-02-20 05:27:15 -0700

    Add addSong and removeSong to PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -1,5 +1,8 @@
 // package model;
 
+import java.util.ArrayList;
+
+
 // PlayList.java
 // Created 2 - 15 - 2025
 // Authors: Lilian and Lucian
@@ -8,10 +11,22 @@
 public class PlayList
 {
 	// INSTANCE VARIABLES
+	private String name;
+	private ArrayList<Song> songs;
 	
 	// CONSTRUCTOR
+	public PlayList(String title) {
+		this.name = title;
+	}
 	
 	// GETTERS+SETTERS
 	
 	// MISC. METHODS
+	public void addSong(Song song) {
+		this.songs.add(song);
+	}
+
+	public void removeSong(Song song) {
+		this.songs.remove(song);
+	}
 }
