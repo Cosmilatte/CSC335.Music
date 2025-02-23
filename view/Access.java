cb6bbc9e75a2c1c9373507feaa625ba2804a2ee0
Cosmilatte <166539222+Cosmilatte@users.noreply.github.com>
2025-02-22 17:03:28 -0700

    Update on Access

--- view/Access.java
+++ view/Access.java
@@ -7,6 +7,7 @@ package view;
 
 import model.LibraryModel;
 import model.MusicStore;
+import java.util.Scanner;
 
 public class Access 
 {
@@ -18,7 +19,8 @@ public class Access
 	// Main
 	public static void main(String[] args)
 	{
-		Access runProgram = new Access();
+		//Access runProgram = new Access();
+		run();
 	}
 	
 	// Constructor
@@ -32,14 +34,48 @@ public class Access
 	
 	
 	// Method
-	private void run()
+	private static void run()
 	{
 		System.out.println("|--======================================--|");
 		System.out.println("\n");
+		System.out.println(" Hello User! Welcome to Lilian and Lucian's ");
+		System.out.println("            MUSIC LIBRARY MODEL!            ");
 		System.out.println("\n");
-		System.out.println("Hello User! Welcome to Lilian and Lucian's");
-		System.out.println("           MUSIC LIBRARY MODEL!           ");
+		System.out.println("|--======================================--|");
 		System.out.println("\n");
+		System.out.println("Please type ''help'' to begin, or whenever you need help! ");
+		
+		Scanner scanner = new Scanner(System.in);
+		String response = scanner.nextLine();
+		if (response.contentEquals("help"))
+			help();
+		
+		while (true)
+		{
+			System.out.println(">: ");
+			response = scanner.nextLine();
+			if (response.contentEquals("help"))
+				help();
+			else
+			{
+				if (response.contentEquals("quit"))
+					return;
+			}
+		}
+	}
+	
+	
+	private static void help()
+	{
+		System.out.println("|--======================================--|");
+		System.out.println("\n");
+		System.out.println("Type 'help' at any time to return to this page.");
+		System.out.println("Type 'quit' at any time to exit the program.");
+		System.out.println("\n");
+		System.out.println("USABLE COMMANDS:");
+		
+		
+		
 		System.out.println("\n");
 		System.out.println("|--======================================--|");
 	}
